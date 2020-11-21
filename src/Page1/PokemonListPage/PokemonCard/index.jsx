3df469882b46bafd46dd214c05cc0c501085273a
rael06 <rael06@hotmail.fr>
2020-11-21 23:52:56 +0100

    update card css and jsx

--- src/Page1/PokemonListPage/PokemonCard/index.jsx
+++ src/Page1/PokemonListPage/PokemonCard/index.jsx
@@ -2,18 +2,18 @@ import PokemonType from 'common/components/PokemonType'
 import React from 'react'
 import padder from 'common/utils/padder'
 import styles from './style.module.css'
-import pokeball from './pokeball.png'
+// import pokeball from './pokeball.png'
 
 export default function PokemonCard({ id, name, image, types }) {
-  const [imageSource, setImageSource] = React.useState(pokeball)
+  // const [imageSource, setImageSource] = React.useState(pokeball)
 
-  React.useEffect(() => setImageSource(image), [image])
+  // React.useEffect(() => setImageSource(image), [image])
 
   return (
     <div className={styles.wrapper}>
       <p className={styles.id}>No.{padder('0', 3, id)}</p>
       <p className={styles.name}>{name}</p>
-      <img className={styles.image} alt="pic" src={imageSource} />
+      <img className={styles.image} alt="pic" src={image} />
       <div className={styles.types}>
         {types.map((type) => (
           <div key={id + name + type} className={styles.type}>

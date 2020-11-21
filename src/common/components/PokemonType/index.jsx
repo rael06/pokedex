a606591bca6593d2274300b3f9e290b7817d6c90
rael06 <rael06@hotmail.fr>
2020-11-21 18:35:09 +0100

    update to official pokemon types colors

--- src/common/components/PokemonType/index.jsx
+++ src/common/components/PokemonType/index.jsx
@@ -4,24 +4,25 @@ import contrastor from 'common/helpers/contrastor'
 
 // const regex = /(?:"types": \["([a-z]+)"(?:, "([a-z]+)")*\],)/
 const typeColors = {
-  fire: 'red',
-  poison: 'purple',
-  grass: 'green',
-  flying: 'lightcyan',
-  water: 'blue',
-  bug: 'orange',
-  normal: 'lightgrey',
-  electric: 'yellow',
-  ground: 'brown',
-  fairy: 'pink',
-  fighting: 'black',
-  psychic: 'pink',
-  rock: 'grey',
-  steel: 'darkgrey',
-  ice: 'lightblue',
-  ghost: 'white',
-  dragon: 'turquoise',
+  poison: '#A33EA1',
+  grass: '#7AC74C',
+  flying: '#A98FF3',
+  fire: '#EE8130',
+  water: '#6390F0',
+  bug: '#A6B91A',
+  normal: '#A8A77A',
+  electric: '#F7D02C',
+  ground: '#E2BF65',
+  fairy: '#D685AD',
+  fighting: '#C22E28',
+  psychic: '#F95587',
+  rock: '#B6A136',
+  ice: '#96D9D6',
+  steel: '#B7B7CE',
+  ghost: '#735797',
+  dragon: '#6F35FC',
 }
+
 export default function PokemonType({ type }) {
   return (
     <div

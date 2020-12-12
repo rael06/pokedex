1ba37562e987fe596e7e74b3e9f77d899446d0ca
rael06 <rael06@hotmail.fr>
2020-12-12 22:12:10 +0100

    step 3

--- src/common/components/NotFound/index.test.jsx
+++ src/common/components/NotFound/index.test.jsx
@@ -3,7 +3,7 @@ import userEvent from '@testing-library/user-event'
 import { createMemoryHistory } from 'history'
 import React from 'react'
 import { Router } from 'react-router-dom'
-
+import translations from 'translations.json'
 import '@testing-library/jest-dom'
 import NotFound from './index'
 import LocaleContext from 'common/contexts/LocaleContext'
@@ -12,16 +12,17 @@ describe('NotFound component', () => {
   it('Should display 404', () => {
     const history = createMemoryHistory()
     const route = '/bad-route'
+    const locale = 'fr'
     history.push(route)
     render(
-      //<LocaleContext.Provider value={}>
-      <Router history={history}>
-        <NotFound />
-      </Router>
-      // </LocaleContext.Provider>
+      <LocaleContext.Provider value={{ locale }}>
+        <Router history={history}>
+          <NotFound />
+        </Router>
+      </LocaleContext.Provider>
     )
     screen.getByText(/404/i)
-    const goBack = screen.getByRole('button', { name: new RegExp(translations['goBack'].en) })
+    const goBack = screen.getByRole('button', { name: new RegExp(translations['goBack'][locale]) })
     userEvent.click(goBack)
     expect(history.location.pathname).toBe('/pokemons')
   })

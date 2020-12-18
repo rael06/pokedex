293f07f831bffbb8083af69f2c6a27b8dd777439
rael06 <rael06@hotmail.fr>
2020-12-18 19:05:17 +0100

    fixed PokemonList test

--- src/PokemonListPage/PokemonList/index.test.jsx
+++ src/PokemonListPage/PokemonList/index.test.jsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom'
-import { getAllByText, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import React from 'react'
 import PokemonList from '.'
 import pokemonList from 'common/models/pokemons.json'
